821312644e2df698911901b94f6486191a21a4fc
Philip <sokolovphilip@gmail.com>
2024-01-11 17:52:13 +0300

    Init

--- bin/console_test.dart
+++ bin/console_test.dart
@@ -1,6 +1,23 @@
+import 'dart:math';
 import 'package:console_test/console_test.dart' as console_test;
 
 void main(List<String> arguments) {
-  print('Putin spent ${console_test
-      .calculateWarBudget()} trillion rubles on the war');
+  print(
+      'Putin spent ${format(console_test.calculateWarBudget(), 2)} trillion rubles on the war');
+}
+
+String format(double d, int fractionDigits) {
+  String res;
+  int tr = d.truncate();
+  if (tr == d) {
+    res = tr.toString();
+  } else {
+    int digits = ((d - tr) * pow(10, fractionDigits)).round();
+    while (digits % 10 == 0) {
+      digits ~/= 10;
+      fractionDigits--;
+    }
+    res = d.toStringAsFixed(fractionDigits);
+  }
+  return res;
 }
--- lib/console_test.dart
+++ lib/console_test.dart
@@ -1,6 +1,7 @@
-int calculateWarBudget() {
+double calculateWarBudget() {
   final startOfWar = DateTime.parse("2022-02-24 06:00:00");
   final now = DateTime.now();
   final res1000x = now.difference(startOfWar).inHours;
-  return (res1000x/1000).round();
+  return (res1000x/1000);
+
 }
